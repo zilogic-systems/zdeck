0d41c66730149a7256fd743be3abc99fb348b787
Vijay Kumar <vijaykumar@bravegnu.org>
2017-07-26 21:49:56 +0530

    Added script to init zdeck presentation folder.

---
+++ zdeck-init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -e ~/.zdeck ]; then
+    source ~/.zdeck
+fi
+
+ZDECK_HOME=${ZDECK_HOME:-/usr/share/zdeck}
+
+if [ -z $1 ]; then
+    echo "Usage: zdeck-init <presentation-format>"
+    echo
+    echo "Supported Formats:" $(ls $ZDECK_HOME/tmpl/)
+fi
+
+if [ -e $ZDECK_HOME/tmpl/$1 ]; then
+    cp -a $ZDECK_HOME/tmpl/$1/* .
+else
+    echo "zdeck-init: incorrect presentation format" 2>&1
+    exit 1
+fi
